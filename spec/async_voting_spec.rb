bd3e853894f896fe43234a737a5c7a39d3286304
nisevi <nicolas.s.vidal@gmail.com>
2018-03-21 08:04:07 +0100

    Testing all md files convertion to html in the root directory.

--- spec/async_voting_spec.rb
+++ spec/async_voting_spec.rb
@@ -2,9 +2,14 @@
 
 require 'spec_helper'
 
-RSpec.describe 'ASYNC_VOTING.md' do
-  it 'should convert markdown to html' do
-    file = File.open('ASYNC_VOTING.md', 'r:UTF-8')
-    expect { Kramdown::Document.new(file.read).to_html }.not_to raise_error
+RSpec.describe 'Process md files in the root path' do
+  markdown_paths = Dir.glob('./*').select { |path| path if path =~ /\.md$/i }
+
+  markdown_paths.each do |markdown_path|
+    it "should convert '#{markdown_path}' markdown to html" do
+      File.open(markdown_path, 'r:UTF-8') do |file|
+        expect { MarkdownConverter.new(file.read).as_html }.not_to raise_error
+      end
+    end
   end
 end
